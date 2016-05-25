1ceb831722f405a4cf93d82403b56dd1adc1dcd9
Christy <511955993@qq.com>
2016-05-25 20:49:33 +0800

    unfinish

--- src/com/edu/interpreter/Main.java
+++ src/com/edu/interpreter/Main.java
@@ -2,8 +2,8 @@ package com.edu.interpreter;
 
 public class Main {
 	public static void main(String[] args) {
-		MoneyContext context = new MoneyContext("one thousand trillion one billion one million forty five thousand three hundred thirty three");
-		//MoneyContext context = new MoneyContext("one thousand");
+		//MoneyContext context = new MoneyContext("one thousand trillion one billion one million forty five thousand three hundred thirty three");
+		MoneyContext context = new MoneyContext("six billion twenty million three hundred thousand five hundred thirty two");
 		TotalExpression expression = new TotalExpression();
 		long total = 0;
 		try {
